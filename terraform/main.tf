92063cb34a46f2f1d36d613c2d96f09c131e6f7c
Viralsuhagiya <viralsuhagiya02@gmail.com>
2023-04-27 13:55:31 +0530

    Revert "added s3 credential using environment variables."
    This reverts commit 80391a3baefea3e2f7ae07b6757cd7c3d25ff50d.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,5 @@
 provider "aws" {
     region = var.region
-    access_key = var.AWS_ACCESS_KEY_ID
-    secret_key = var.AWS_SECRET_ACCESS_KEY
-    token = var.AWS_SESSION_TOKEN
 }
 
 module "s3_module" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,3 @@
 variable env {}
 variable region {}
 variable app_name {}
-variable AWS_SESSION_TOKEN {}
-variable AWS_ACCESS_KEY_ID {}
-variable AWS_SECRET_ACCESS_KEY {}
